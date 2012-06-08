a4ab0c7a75e724939c6c067bfc6881f67d4b0cee
Nico Sztabzyb <nico@szstudios.net>
2012-06-08 19:44:36 -0300

    OMG the clouds are moving.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,34 @@
-$(function() {
+function CloudInit() {
+  var width = $(window).width() + 'px top';  
+  $('.nube#uno').animate({
+   marginTop: "40px",
+   backgroundPosition: width
+  }, 73000);
+  setTimeout("CloudReset()", 53100 );
+}
+function CloudReset() {
+  $('.nube#uno').css({marginTop: '50px', backgroundPosition: '-100px top'});
+  setTimeout("CloudInit()", 7000 );
+}
+
+
+function CloudInit2() {
+  var width = $(window).width() + 'px top';  
+  $('.nube#dos').delay(10000).css({ opacity: 0.8 }).animate({
+   backgroundPosition: width
+  }, 83000);
+  setTimeout("CloudReset2()", 83100 );
+}
+function CloudReset2() {
+  $('.nube#dos').css({backgroundPosition: '-100px top'});
+  setTimeout("CloudInit2()", 12000 );
+}
+
+
+$(window).load(function() {   
+  CloudInit();
+  CloudInit2();
   
 });
+
+
